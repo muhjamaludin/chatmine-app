86b06521e7d1258089637a742b7d9eba519c0264
muhjamaludin <muhammad1400ft2015@student.uny.ac.id>
2020-04-22 23:23:06 +0700

    configuration redux

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,15 +1,7 @@
 import {combineReducers} from 'redux';
 
-const chatList = [
-  {
-    id: 1,
-    name: 'Susanna',
-    messsage: [
-      {
-        id: 182930,
-        body: 'Hello there',
-        date: 1587440984261,
-      },
-    ],
-  },
-];
+import Auth from './Auth';
+
+export default combineReducers({
+  auth: Auth,
+});
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,23 @@
-import {createStore, applyMiddleware} from 'redux'
-import logger from 'redux-logger'
-import {} from 'redux-promise-middleware'
+import {createStore, applyMiddleware} from 'redux';
+import logger from 'redux-logger';
+import promiseMiddleware from 'redux-promise-middleware';
+import storage from 'redux-persist/lib/storage';
+import {persistStore, persistReducer} from 'redux-persist';
+import thunk from 'redux-thunk';
+import reducer from './reducer/index';
 
-const 
+const config = {
+  key: 'chatmine',
+  storage,
+};
+
+const persistedReducer = persistReducer(config, reducer);
+
+export default function () {
+  const store = createStore(
+    persistedReducer,
+    applyMiddleware(promiseMiddleware, logger, thunk),
+  );
+  const persistor = persistStore(store);
+  return {store, persistor};
+}
